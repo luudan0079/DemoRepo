137163136b4df4ddbcdaeec971474fec8c37a88a
luudan0079 <haibt123@gmail.com>
2022-02-15 10:08:01 +0700

    feat: test1

--- app/src/main/java/com/hbt/demoapplication/MainActivity.kt
+++ app/src/main/java/com/hbt/demoapplication/MainActivity.kt
@@ -31,10 +31,6 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        // Using
-        thread {
-            // Do something
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
